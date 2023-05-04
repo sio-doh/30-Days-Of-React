8446c1f0797051c598b4bd649a5472f63a470ed1
Siobhan Doherty <siobhandoherty18@gmail.com>
2023-05-04 16:05:58 +0200

    exercise-day3 complete

--- solutions/day-03/exercises_level_2/src/index.js
+++ solutions/day-03/exercises_level_2/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom"; 
+import { FaHtml5, FaCss3Alt, FaJsSquare, FaReact } from "react-icons/fa";
 
 const title = "SUBSCRIBE";
 const tagline = "Sign up with your email address to receive news and updates";
@@ -8,109 +9,118 @@ const placeholder_two = "Last Name";
 const placeholder_three = "Email";
 const subscribe_button = "Subscribe";
 
+const FrontEndTechnologies = () => {
+    return (
+        <div 
+            className="frontend-container" 
+            style={{
+                alignItems: "center",
+                display: "flex",
+                flexDirection: "column", 
+                justifyContent: "center", 
+                margin: "0 auto", 
+                width: "80%",
+            }}
+        >
+            <h1 style={{ fontWeight: "bold", fontSize: "2rem", marginBottom: "2rem" }}>
+                Front End Technologies
+            </h1>
+            <div className="icon-container">
+                <FaHtml5 size={60} style={{ margin: "1rem" }}/>
+                <FaCss3Alt size={60} style={{ margin: "1rem" }}/>
+                <FaJsSquare size={60} style={{ margin: "1rem" }}/>
+                <FaReact size={60} style={{ margin: "1rem" }}/>
+            </div>
+        </div>
+    );
+};
+
 const app = (
-    <div
-        className="container"
-        style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            width: "80%",
-            height: "300px",
-            background: "#ADD8E6",
-            fontFamily: "Montserrat",
-        }}
-    >
-    <div
-        className="text"
-        style={{
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-            paddingBottom: 20,
-        }}
-    >
-        <h1 style={{ fontWeight: 235, letterSpacing: -2 }}>{title}</h1>
-        <p>{tagline}</p>
-    </div>
-    <div
-        className="input_tags"
-        style={{
-            display: "flex",
-            alignItems: "center",
-            justifyContent: "flex-end",
-        }}
-    >
-        <input
-            type="text"
-            placeholder={placeholder_one}
+    <div className="main">
+        <FrontEndTechnologies />
+        <div
+            className="subscribe"
             style={{
-                padding: 10,
-                margin: 10,
-                border: "1px lightgrey solid",
-                borderRadius: 10,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                width: "80%",
+                height: "300px",
+                background: "#ADD8E6",
+                fontFamily: "Montserrat", 
+                margin: "0 auto",
             }}
-        />
-        <input
-            type="text"
-            placeholder={placeholder_two}
+        >
+        <div
+            className="text"
             style={{
-                padding: 10,
-                margin: 10,
-                border: "1px lightgrey solid",
-                borderRadius: 10,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+                paddingBottom: 20,
             }}
-        />
-        <input
-            type="email"
-            placeholder={placeholder_three}
-                style={{
-                padding: 10,
-                margin: 10,
-                border: "1px lightgrey solid",
-                borderRadius: 10,
-            }}
-        />
+        >
+            <h1 style={{ fontWeight: 235, letterSpacing: -2 }}>{title}</h1>
+            <p>{tagline}</p>
         </div>
-        <div className="btn" style={{ padding: 10 }}>
-            <button
+        <div
+            className="input_tags"
+            style={{
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "flex-end",
+            }}
+        >
+            <input
+                type="text"
+                placeholder={placeholder_one}
                 style={{
-                height: 40,
-                width: 200,
-                border: "1px lightgrey solid",
-                borderRadius: 10,
-                background: "#f5837d",
-                color: "white",
+                    padding: 10,
+                    margin: 10,
+                    border: "1px lightgrey solid",
+                    borderRadius: 10,
                 }}
-            >
-                {subscribe_button}
-            </button>
-        </div>
+            />
+            <input
+                type="text"
+                placeholder={placeholder_two}
+                style={{
+                    padding: 10,
+                    margin: 10,
+                    border: "1px lightgrey solid",
+                    borderRadius: 10,
+                }}
+            />
+            <input
+                type="email"
+                placeholder={placeholder_three}
+                    style={{
+                    padding: 10,
+                    margin: 10,
+                    border: "1px lightgrey solid",
+                    borderRadius: 10,
+                }}
+            />
+            </div>
+            <div className="btn" style={{ padding: 10 }}>
+                <button
+                    style={{
+                    height: 40,
+                    width: 200,
+                    border: "1px lightgrey solid",
+                    borderRadius: 10,
+                    background: "#f5837d",
+                    color: "white",
+                    }}
+                >
+                    {subscribe_button}
+                </button>
+            </div>
+        </div>      
     </div>
 );
-
-
-
-// const app = (
-//     <div className="app">
-//         <div>
-//             <h1>{title}</h1>
-//             <p>{tagline}</p>
-//         </div>
-//         <div>
-//             <input placeholder={placeholder_one}/>
-//             <input placeholder={placeholder_two} /> 
-//             <input placeholder={placeholder_three} />
-//         </div>
-//         <div>
-//             <button>
-//                 {subscribe_button}
-//             </button>
-//         </div>
-//     </div>
-// );
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(app, rootElement);
--- solutions/day-03/exercises_level_3/src/index.js
+++ solutions/day-03/exercises_level_3/src/index.js
@@ -6,12 +6,26 @@ import { AiOutlineClockCircle } from "react-icons/ai";
 
 const image = (
   <div>
-    <img src={asabeneh} alt="Asabeneh img" style={{ height: "225px", borderRadius: 150, marginLeft: 5 }} />
+    <img
+      src={asabeneh}
+      alt="Asabeneh img"
+      style={{ height: "225px", borderRadius: 150, marginLeft: 5 }}
+    />
   </div>
 );
+
 const user = (
-  <h2>
-    ASABENEH YETAYEH <BsCheckCircleFill style={{ backgroundColor: "#"  , color: "#40E0D0", marginLeft: 10, borderRadius: 10 }} className="bscheckcircle" />
+  <h2 style={{ display: "inline-flex", alignItems: "center" }}>
+    ASABENEH YETAYEH{" "}
+    <BsCheckCircleFill
+      style={{
+        backgroundColor: "#",
+        color: "#40E0D0",
+        marginLeft: 10,
+        borderRadius: 10,
+      }}
+      className="bscheckcircle"
+    />
   </h2>
 );
 
@@ -41,9 +55,34 @@ const skillset = [
   "Git"
 ];
 
-const skills = skillset.map((skill, index) => <p key={index}>{skill}</p>);
+const Skills = () => (
+  <div className="skillset">
+    <h2>SKILLS</h2>
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      {skillset.map((skill) => (
+        <div
+          key={skill}
+          style={{
+            padding: 5,
+            margin: 10,
+            border: "2px solid #40E0D0",
+            borderRadius: 10,
+            background: "#40E0D0",
+            color: "white",
+          }}
+        >
+          {skill}
+        </div>
+      ))}
+    </div>
+  </div>
+);
 
-const date = <p><AiOutlineClockCircle style={{ paddingRight: 10 }} />Joined on Aug 30, 2020</p>;
+const date = (
+  <p>
+    <AiOutlineClockCircle style={{ paddingRight: 10 }} />Joined on Aug 30, 2020
+  </p>
+); 
 
 const ErrorBoundary = class extends React.Component {
   constructor(props) {
@@ -59,7 +98,7 @@ const ErrorBoundary = class extends React.Component {
     }
     return this.props.children;
   }
-}
+};
 
 const app = (
   <ErrorBoundary>
@@ -69,29 +108,17 @@ const app = (
         <p style={{ color: "#000000", marginLeft: 10 }}>{user}</p>
         <p style={{ marginLeft: 10, fontWeight: 1000 }}>{position}</p>
       </div>
-      <div style={{ display: "flex", marginLeft: 10 }}> 
-        <h2>SKILLS</h2>
-        <div 
-          className="skillset"
-          style={{ 
-            padding: 5,
-            margin: 10,
-            border: "2px solid #40E0D0",
-            borderRadius: 10,
-            background: "#40E0D0",
-            color: "white",
-          }}
-        >
-          {skills}</div>
-      </div>
+      <Skills />
       <footer>
-        <div 
+        <div
           style={{
             marginLeft: 10,
             display: "flex",
             alignItems: "center",
           }}
-        >{date}</div>
+        >
+          {date}
+        </div>
       </footer>
     </div>
   </ErrorBoundary>
